510fcfc2baefd719ec9fd9eacc90ada5024e3da1
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-04-20 20:11:50 -0400

    Configure project with react and redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './main.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-document.write('Hello React/Redux!')
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
+
+import thunk from 'redux-thunk'
+import reducers from 'reducers'
+
+
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
