78762e75096b3cf25c8c0258996db65d2f48d4b7
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-04-28 09:31:24 -0400

    Phone detail pages

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import reducers from 'reducers'
 
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
+import Phone from 'containers/phone'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -26,6 +27,8 @@ ReactDOM.render(
             <Route component={Layout}>
                 <Route path='/' component={Phones} />
             </Route>
+
+            <Route path = '/phones/:id' component = {Phone}/>
         </Router>
     </Provider>,
     document.getElementById('root')
